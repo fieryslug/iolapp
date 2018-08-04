eea967a3260e6e311fc1d007e7ae1c3784d4ce6a
fieryslug <mike.wang96029@gmail.com>
2018-08-05 00:36:18 +0800

    basic things

--- app/src/main/java/com/iol/app/MainActivity.java
+++ app/src/main/java/com/iol/app/MainActivity.java
@@ -1,13 +1,36 @@
 package com.iol.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initialize();
+    }
+
+    private void initialize() {
+
+        buttonStart = findViewById(R.id.bt_main_start);
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
---
+++ developer_norm.md
@@ -0,0 +1,14 @@
+# NORM
+
+## 1. package management
+
+-activities for individual features are sorted under activity/ package
+
+
+## 2. Naming convention
+
+-class: upper camel case
+  e.g. CustomButton
+
+-field/method: lower camel case
+  e.g. buttonRed
